64328f1aff6d71487c1da3f338e0f705e38fb7e4
Miroslav Kravec <kravec.miroslav@gmail.com>
2016-11-11 10:36:03 +0100

    Added Fluxbox configuration; ssh-agent; and, some fine-tuning

--- configure.sh
+++ configure.sh
@@ -33,6 +33,9 @@ function install_link() {
 }
 
 install_link "$PWD/bashrc" "$HOME/.bashrc"
+install_link "$PWD/fluxbox/overlay" "$HOME/.fluxbox/overlay"
+install_link "$PWD/fluxbox/startup" "$HOME/.fluxbox/startup"
+install_link "$PWD/ssh/config" "$HOME/.ssh/config"
 install_link "$PWD/tint2rc" "$HOME/.config/tint2/tint2rc"
 install_link "$PWD/vimrc" "$HOME/.vimrc"
 install_link "$PWD/vim" "$HOME/.vim"
