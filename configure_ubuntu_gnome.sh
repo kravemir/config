b1398a85064b65d16a4679482514479d9cee64f3
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-12-02 10:05:19 +0100

    Ubuntu: disable application switch with super key

---
+++ configure_ubuntu_gnome.sh
@@ -0,0 +1,5 @@
+
+# source: https://askubuntu.com/questions/968103/disable-the-app-key-supernum-default-functionality-in-ubuntu-17-10
+
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+
